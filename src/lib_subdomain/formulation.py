e4624ad694b2b190aa93b025ff12ddf582078413
Boris Martin <boris.martin@uliege.be>
2025-11-20 13:52:22 +0100

    untested ddm operator

--- src/lib_subdomain/formulation.py
+++ src/lib_subdomain/formulation.py
@@ -10,6 +10,8 @@ from numpy import conjugate
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import LinearOperator, spsolve
 
+import petsc4py.PETSc as PETSc
+
 @BilinearForm
 def helmholtz(u, v, w):
     k = w["k"]
@@ -55,28 +57,112 @@ class Formulation:
         self.domains: SubdomainsOnMyRank = domains
         self.k = k
         self.volume_mats: list[csr_matrix]
+        self.local_masses: dict[tuple[int, int], csr_matrix] = {}
+        self.local_transmission: dict[tuple[int, int], csr_matrix] = {}
+        self.swap: PETSc.Mat = domains.build_swap_operator()
 
     def assemble_volume(self):
         self.volume_mats = []
         for dom in self.domains.subdomains:
             A = skfem.asm(helmholtz, dom.volume_basis , k=self.k)
             if dom.has_gamma():
-                A -= skfem.asm(absorbing, dom.gamma_basis, k=self.k)
+                A += skfem.asm(absorbing, dom.gamma_basis, k=self.k)
             for _, basis in dom.sigma_basis.items():
-                A -= skfem.asm(transmission, basis, k=self.k)
+                A += skfem.asm(transmission, basis, k=self.k)
             self.volume_mats.append(A.tocsr())
+        assert len(self.volume_mats) == len(self.domains.subdomains)
 
+    def assemble_interface_mats(self):
+        for ki, i in enumerate(self.domains.partitions):
+            for j in self.domains.subdomains[ki].all_neighboring_partitions():
+                if (i, j) in self.local_masses:
+                    raise ValueError("Interface matrices already assembled")
+                M = skfem.asm(
+                    mass_bnd,
+                    self.domains.subdomains[ki].sigma_basis[j],
+                    k=self.k,
+                )
+                S = skfem.asm(
+                    transmission,
+                    self.domains.subdomains[ki].sigma_basis[j],
+                    k=self.k,
+                )
+                gdofs = self.domains.subdomains[ki].dofs_on_interface(j)
+                M_reduced = M[gdofs, :][:, gdofs]
+                S_reduced = S[gdofs, :][:, gdofs]
+                self.local_masses[(i, j)] = M_reduced.tocsr()
+                self.local_transmission[(i, j)] = S_reduced.tocsr()
+
+    def apply_scatter(self, x: PETSc.Vec, y: PETSc.Vec):
+        """
+        Scatter from global g-vector x to local subdomain vectors y.
+        """
+        self.swap.mult(x, y)
+        y_numpy = y.getArray()
+
+        offsets = self.domains.local_offset_list()
+
+        for iidx, i in enumerate(self.domains.partitions):
+            dom = self.domains.subdomains[iidx]
+            offset = self.domains.offset_of_domain(i)
+            g_size = self.domains.g_vector_size_for_domain(i)
+            print("For domain ", i, " g-size=", g_size)
+
+            rhs = np.zeros(dom.volume_size(), dtype=np.complex128)
+            for j in dom.all_neighboring_partitions():
+                # find idx in the offsets list
+                for idx in range(len(offsets[0])):
+                    if offsets[0][idx] == i and offsets[1][idx] == j:
+                        local_offset = offsets[2][idx]
+                        break
+                dofs = dom.dofs_on_interface(j)
+                print(dofs)
+                print("Dofs shape is ", len(dofs))
+                print("Local mss shape is ", self.local_masses[(i, j)].shape)
+                print("y_numpy shape is ", y_numpy[local_offset:local_offset+len(dofs)].shape)
+                mass_g = self.local_masses[(i, j)] @ y_numpy[local_offset:local_offset+len(dofs)]
+                rhs[dofs] += mass_g
+                if (PETSc.COMM_WORLD.rank == 0):
+                    print(f"Applying interface from {j} to {i} at local offset {local_offset} (global offset {offset + local_offset})")
+
+            print("Attempting to read entry ", iidx, " of volume mats of size ", len(self.volume_mats))
+            u_i = spsolve(self.volume_mats[iidx], rhs)
+
+            y_numpy[:] *= -1 # g_ij = -g_ij
+            for j in dom.all_neighboring_partitions():
+                # find idx in the offsets list
+                for idx in range(len(offsets[0])):
+                    if offsets[0][idx] == i and offsets[1][idx] == j:
+                        local_offset = offsets[2][idx]
+                        break
+                dofs = dom.dofs_on_interface(j)
+                su = self.local_transmission[(i, j)] @ u_i[dofs]
+                m_inv_su = spsolve(self.local_masses[(i, j)], su)
+                y_numpy[local_offset:local_offset+len(dofs)] += 2 * m_inv_su
+
+
+
+        y.setArray(y_numpy)
 
 if __name__ == "__main__":
-    ndom = 4
+    ndom = 8
     subdomains_on_rank = SubdomainsOnMyRank(ndom)
     create_square(0.04, ndom)
     for dom in subdomains_on_rank.subdomains:
         dom.init_all()
 
     print("Partitions on this rank:", subdomains_on_rank.partitions)
+    for i in subdomains_on_rank.partitions:
+        offset = subdomains_on_rank.offset_of_domain(i)
+        print(f"Domain {i} offset in g-vector:", offset)
     for dom in subdomains_on_rank.subdomains:
         print(f"Domain {dom.partition} neighbors:", dom.all_neighboring_partitions())
 
     formulation = Formulation(subdomains_on_rank, k=10.0)
     formulation.assemble_volume()
+    formulation.assemble_interface_mats()
+    x = subdomains_on_rank.create_petsc_g_vector()
+    x.set(PETSc.COMM_WORLD.rank+1)
+    y = x.duplicate()
+    formulation.apply_scatter(x, y)
+    y.view()
